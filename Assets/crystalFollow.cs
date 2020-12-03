60dbd7bc7eacd38b62102538abc9014bef490745
Dame <dame.arias@gmail.com>
2020-12-03 02:52:24 -0500

    Progress

---
+++ Assets/crystalFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crystalFollow : MonoBehaviour
+{
+    public Transform spiderTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (spiderTrans.position.x, spiderTrans.position.y + 0.6f, spiderTrans.position.z);
+    }
+}
